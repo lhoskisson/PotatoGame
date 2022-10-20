19d635f25f60d6d6558706052673a6b4817d93d1
Andres Marin <amarin03@uw.edu>
2022-10-20 16:01:08 -0700

    Farmer movement added

---
+++ Assets/FarmerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmerMovement : MonoBehaviour
+{
+    
+    public float speed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKey("w")){
+            transform.position += transform.up * (speed * Time.smoothDeltaTime);
+        }
+            
+        if (Input.GetKey("s")){
+            transform.position -= transform.up * (speed * Time.smoothDeltaTime);
+
+        }
+            
+        if (Input.GetKey("a")){
+            transform.Translate(Vector3.left * (speed * Time.smoothDeltaTime));
+            Quaternion farmerRotation = Quaternion.LookRotation(Vector3.left);
+
+        }
+            
+        if (Input.GetKey("d")){
+            transform.Translate(Vector3.right * (speed * Time.smoothDeltaTime)); 
+            Quaternion farmerRotation = Quaternion.LookRotation(Vector3.right);
+
+        }
+    }
+}
