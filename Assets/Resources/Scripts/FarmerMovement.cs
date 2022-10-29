0938ae42f30681fb786b04db9d33e04607a6fadc
Andres Marin <amarin03@uw.edu>
2022-10-28 22:20:32 -0700

    Added minimap and farmer rotation

---
+++ Assets/Resources/Scripts/FarmerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmerMovement : MonoBehaviour
+{
+    
+    public float speed = 10f;
+
+    void Update(){
+        Movement();
+		/* //Rotating Camera
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0f;
+        transform.up = mouse - transform.position; */
+
+    }
+    public void Movement(){
+        if (Input.GetKey("w")){
+            transform.position += transform.up * (speed * Time.smoothDeltaTime);
+        }
+            
+        if (Input.GetKey("s")){
+            transform.position -= transform.up * (speed * Time.smoothDeltaTime);
+
+        }
+            
+        if (Input.GetKey("a")){
+            transform.Translate(Vector3.left * (speed * Time.smoothDeltaTime));
+            // Quaternion farmerRotation = Quaternion.LookRotation(Vector3.left);
+
+        }
+            
+        if (Input.GetKey("d")){
+            transform.Translate(Vector3.right * (speed * Time.smoothDeltaTime)); 
+            // Quaternion farmerRotation = Quaternion.LookRotation(Vector3.right);
+
+        }      
+    }
+}
