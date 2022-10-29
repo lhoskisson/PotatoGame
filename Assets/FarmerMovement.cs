e60feee795c658645cf659820a724a25a257c452
Andres Marin <amarin03@uw.edu>
2022-10-28 22:21:07 -0700

    added the teams assets

--- Assets/FarmerMovement.cs
+++ Assets/FarmerMovement.cs
@@ -11,37 +11,45 @@ public class FarmerMovement : MonoBehaviour
 
         Movement();
 <<<<<<< Updated upstream:Assets/FarmerMovement.cs
+<<<<<<< Updated upstream:Assets/FarmerMovement.cs
 =======
 		MouseRotation();
+>>>>>>> Stashed changes:Assets/Resources/Scripts/FarmerMovement.cs
+=======
+        MouseRotation();
 >>>>>>> Stashed changes:Assets/Resources/Scripts/FarmerMovement.cs
 
     }
     public void Movement(){
+
+        //Uses WASD keys to move farmer
         if (Input.GetKey("w")){
             transform.position += transform.up * (speed * Time.smoothDeltaTime);
         }
             
         if (Input.GetKey("s")){
             transform.position -= transform.up * (speed * Time.smoothDeltaTime);
-
         }
             
         if (Input.GetKey("a")){
             transform.Translate(Vector3.left * (speed * Time.smoothDeltaTime));
-            // Quaternion farmerRotation = Quaternion.LookRotation(Vector3.left);
-
         }
             
         if (Input.GetKey("d")){
             transform.Translate(Vector3.right * (speed * Time.smoothDeltaTime)); 
-            // Quaternion farmerRotation = Quaternion.LookRotation(Vector3.right);
-
         }      
     }
     public void MouseRotation(){
         
+<<<<<<< Updated upstream:Assets/FarmerMovement.cs
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.z = 0f;
         transform.up = mouse - transform.position;
+=======
+        //Rotating Camera
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0f;
+        transform.up = mouse - transform.position; 
+>>>>>>> Stashed changes:Assets/Resources/Scripts/FarmerMovement.cs
     }
 }
