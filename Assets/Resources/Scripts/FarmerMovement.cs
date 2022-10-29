035acb32840c90ba773f114bc797f55b5c653aba
Andres Marin <amarin03@uw.edu>
2022-10-28 22:31:49 -0700

    Update FarmerMovement.cs

--- Assets/Resources/Scripts/FarmerMovement.cs
+++ Assets/Resources/Scripts/FarmerMovement.cs
@@ -27,13 +27,11 @@ public class FarmerMovement : MonoBehaviour
             
         if (Input.GetKey("a")){
             transform.Translate(Vector3.left * (speed * Time.smoothDeltaTime));
-            // Quaternion farmerRotation = Quaternion.LookRotation(Vector3.left);
 
         }
             
         if (Input.GetKey("d")){
             transform.Translate(Vector3.right * (speed * Time.smoothDeltaTime)); 
-            // Quaternion farmerRotation = Quaternion.LookRotation(Vector3.right);
 
         }      
     }
