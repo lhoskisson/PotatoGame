caf28863b07dfe019b687b865e3b617e628d1158
Andres Marin <amarin03@uw.edu>
2022-11-10 15:27:42 -0800

    Added Knock back features

--- Assets/Resources/Scripts/FarmerMovement.cs
+++ Assets/Resources/Scripts/FarmerMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class FarmerMovement : MonoBehaviour
 {
-
+    public Rigidbody2D rb;
     public float speed = 10f;
-
+    
+    
+    
     void Update(){
         Movement();
 		MouseRotation();
+
 
     }
     public void Movement(){
@@ -27,4 +30,30 @@ public class FarmerMovement : MonoBehaviour
         mouse.z = 0f;
         transform.up = mouse - transform.position;
     }
+    void OnTriggerEnter2D(Collider2D collision){
+
+        float force = 2f;
+
+        if (collision.gameObject.CompareTag("Enemy")){
+
+            rb = GetComponent<Rigidbody2D>();
+            Rigidbody2D enemy = collision.GetComponent<Rigidbody2D>();
+            if (enemy != null){
+                enemy.isKinematic = false;
+                Vector3 distance = transform.position - enemy.transform.position;
+                distance = distance.normalized * force;
+                rb.AddForce(distance, ForceMode2D.Impulse);
+                StartCoroutine(timer(rb));
+                enemy.isKinematic = true;
+                
+            }
+        }
+    }
+    private IEnumerator timer(Rigidbody2D farmer){
+
+        float pushTimer = .5f;
+        yield return new WaitForSeconds(pushTimer);
+        farmer.velocity = Vector3.zero;
+    }
+   
 }
