98e4d4c289884d54876b4007a52401be6d094556
lhoskisson <72849914+lhoskisson@users.noreply.github.com>
2022-11-21 09:25:20 -0800

    Adding Level Manager

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject potatoManager;
+    public GameObject potatoGun;
+
+    private bool initPotatoesSpawned = false;
+
+    void Start()
+    {
+        if (potatoManager == null)
+            potatoManager = GameObject.FindWithTag("Potato Manager");
+    }
+
+    void Update()
+    {
+        if (!initPotatoesSpawned)
+            SpawnInitPotatoes();
+
+        //lose condition
+        if (potatoGun.GetComponent<PotatoGunScript>().getAmmoCount() == 0 && potatoManager.GetComponent<PotatoManager>().PotatoCount() == 0)
+            Timer.levelTime = 0;
+    }
+
+    private void SpawnInitPotatoes()
+    {
+        PotatoManager pm = potatoManager.GetComponent<PotatoManager>();
+        if (!pm.initialized)
+            return;
+        pm.SpawnPotatoes(new Vector3(0, 0, 1), 5, 100);
+        initPotatoesSpawned = true;
+    }
+}
