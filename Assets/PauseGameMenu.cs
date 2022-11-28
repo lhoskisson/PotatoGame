704cb1a22504117675ed0f9a8f186a77590c6428
Andres Marin <amarin03@uw.edu>
2022-11-27 16:51:46 -0800

    Created Pause menu

---
+++ Assets/PauseGameMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGameMenu : MonoBehaviour
+{
+    public static bool pauseGame = false;
+    public GameObject menu;
+    public GameObject canvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p")){
+            if(pauseGame){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+    public void Resume(){
+        menu.SetActive(false);
+        canvas.SetActive(true);
+        Time.timeScale = 1f;
+        pauseGame = false;
+    }
+    public void Pause(){
+        menu.SetActive(true);
+        canvas.SetActive(false);
+        Time.timeScale = 0f;
+        pauseGame = true;
+    }
+    public void QuitGame(){
+        SceneManager.LoadScene("Menu");
+    }
+}
