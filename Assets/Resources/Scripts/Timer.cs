1bc54079798ce7bd32e767732f77dfeda2fdc4c1
lhoskisson <72849914+lhoskisson@users.noreply.github.com>
2022-12-01 17:25:42 -0800

    moved time=0 handle to level manager

--- Assets/Resources/Scripts/Timer.cs
+++ Assets/Resources/Scripts/Timer.cs
@@ -12,8 +12,6 @@ public class Timer : MonoBehaviour
 	
 	public GameObject timeText;
 
-	public GameObject gameOverScreen;
-
     void Start()
     {
         timeText = GameObject.Find("TimeText");
@@ -29,22 +27,6 @@ public class Timer : MonoBehaviour
 		if(((int) previousTime) - ((int) levelTime) > 0)
 		{
 			timeText.GetComponent<Text>().text = ((int) levelTime).ToString();
-		}
-		
-		//if time up, end level
-		if(levelTime <= 0)
-		{
-			//stop enemies
-			
-			
-			//harvest all potato crops
-			GameObject[] potatoManagers = GameObject.FindGameObjectsWithTag("Potato Manager");
-			foreach(GameObject pm in potatoManagers)
-				pm.GetComponent<PotatoManager>().HarvestAllPotatoes();
-
-			//TEMP: game over menu
-			gameOverScreen.SetActive(true);
-			//go to next level
 		}
     }
 }
