fbe1ffc58eff2db512db2fde931eacf5eb2f629f
lhoskisson <72849914+lhoskisson@users.noreply.github.com>
2022-12-01 17:27:11 -0800

    Added time=0 handle, and scene transitions, and ammo tracking between levels

--- Assets/Resources/Scripts/LevelManager.cs
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public int initialCropRows;
+    public int initialCropColumns;
+
     public GameObject potatoManager;
     public GameObject potatoGun;
+    public GameObject gameOverScreen;
 
     private bool initPotatoesSpawned = false;
     private bool gameOver = false;
+    private const int defaultAmmo = 250;
+    private static int ammoTracker = defaultAmmo; //tracks ammoCount between levels
 
     void Start()
     {
+        potatoGun.GetComponent<PotatoGunScript>().setAmmoCount(ammoTracker);
+        Timer.levelTime = Timer.startLevelTime;
         if (potatoManager == null)
             potatoManager = GameObject.FindWithTag("Potato Manager");
     }
@@ -21,11 +30,75 @@ public class LevelManager : MonoBehaviour
         if (!initPotatoesSpawned)
             SpawnInitPotatoes();
 
+        //Dev Keys for switching between levels
+        if(Input.GetKeyDown("]"))
+        {
+            //advance one level
+            Timer.levelTime = 0;
+        }
+        if(Input.GetKeyDown("["))
+        {
+            Debug.Log(SceneManager.GetActiveScene().name);
+            //go back one level
+            if (SceneManager.GetActiveScene().name == "Level 1")
+            {
+                ResetAmmoTracker();
+                Timer.levelTime = Timer.startLevelTime;
+                SceneManager.LoadScene("Menu");
+            }
+            else if (SceneManager.GetActiveScene().name == "Level 2")
+            {
+                TrackAmmo();
+                Timer.levelTime = Timer.startLevelTime;
+                SceneManager.LoadScene("Level 1");
+            }
+            else if (SceneManager.GetActiveScene().name == "Level 3")
+            {
+                TrackAmmo();
+                Timer.levelTime = Timer.startLevelTime;
+                SceneManager.LoadScene("Level 2");
+            }
+        }
+
         //lose condition
         if (!gameOver && /*potatoGun.GetComponent<PotatoGunScript>().getAmmoCount() == 0 &&*/ potatoManager.GetComponent<PotatoManager>().PotatoCount() == 0)
         {
             Timer.levelTime = 0;
+            ResetAmmoTracker();
             gameOver = true;
+            gameOverScreen.SetActive(true);
+        }
+
+        //handle end of level
+        if (Timer.levelTime <= 0)
+        {
+            //harvest all potato crops
+            GameObject[] potatoManagers = GameObject.FindGameObjectsWithTag("Potato Manager");
+            foreach (GameObject pm in potatoManagers)
+                pm.GetComponent<PotatoManager>().HarvestAllPotatoes();
+
+            TrackAmmo();
+            
+            //TODO: Add Next Level Menu for inbetween levels
+
+            //go to next level
+            if(SceneManager.GetActiveScene().name == "Level 1")
+            {
+                Timer.levelTime = Timer.startLevelTime;
+                SceneManager.LoadScene("Level 2");
+            }
+            else if(SceneManager.GetActiveScene().name == "Level 2")
+            {
+                Timer.levelTime = Timer.startLevelTime;
+                SceneManager.LoadScene("Level 3");
+            }
+            //TODO: Add win screen upon completing level 3
+            else
+            {
+                ResetAmmoTracker();
+                Timer.levelTime = Timer.startLevelTime;
+                SceneManager.LoadScene("Menu");
+            }
         }
     }
 
@@ -34,7 +107,18 @@ public class LevelManager : MonoBehaviour
         PotatoManager pm = potatoManager.GetComponent<PotatoManager>();
         if (!pm.initialized)
             return;
-        pm.SpawnPotatoesRectangle(new Vector3(0, 0, 1), 7, 7);
+        pm.SpawnPotatoesRectangle(new Vector3(0, 0, 1), initialCropRows, initialCropColumns);
         initPotatoesSpawned = true;
+    }
+
+    private void TrackAmmo()
+    {
+        //record ammoCount
+        ammoTracker = potatoGun.GetComponent<PotatoGunScript>().getAmmoCount();
+    }
+
+    private void ResetAmmoTracker()
+    {
+        ammoTracker = defaultAmmo;
     }
 }
